d38be55aa40d864f200c573af6b33e9499d41511
Andy Ibanez <leonnears@gmail.com>
2015-06-08 21:38:59 -0400

    Agregado Parse como librería y como dependencia. Y FUNCIONANDO :D.

---
+++ app/src/main/java/edu/upb/omaigad/upbmatch/upb_match/TeamsManager.java
@@ -0,0 +1,7 @@
+package edu.upb.omaigad.upbmatch.upb_match;
+
+/**
+ * Created by andyibanezk on 6/8/15.
+ */
+public class TeamsManager extends Object {
+}
---
+++ app/src/main/java/edu/upb/omaigad/upbmatch/upb_match/UPBMatchApplication.java
@@ -0,0 +1,17 @@
+package edu.upb.omaigad.upbmatch.upb_match;
+import android.app.Application;
+import com.parse.*;
+
+/**
+ * Created by andyibanezk on 6/8/15.
+ */
+
+public class UPBMatchApplication extends Application {
+    public void onCreate() {
+        super.onCreate();
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "vCDljkDZu04YLITBMCjZdWngHE4RU0ojdqmySUd4", "jwHg4psPxJfToPQk4lVj2rPztrD3Q13Io2KmoUzv");
+
+    }
+}
