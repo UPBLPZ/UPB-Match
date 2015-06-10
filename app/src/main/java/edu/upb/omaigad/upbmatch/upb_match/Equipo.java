821a2eb07c9e861a5841964cd1281008fceead63
Andy Ibanez <leonnears@gmail.com>
2015-06-10 00:26:29 -0400

    Todo lo que hay que hacer es manera ese ParseObject a instancias de Teams y ya casi está... Ya casi.

--- app/src/main/java/edu/upb/omaigad/upbmatch/upb_match/Equipo.java
+++ app/src/main/java/edu/upb/omaigad/upbmatch/upb_match/Equipo.java
@@ -4,4 +4,25 @@ package edu.upb.omaigad.upbmatch.upb_match;
  * Created by andyibanezk on 6/9/15.
  */
 public class Equipo {
+    private String color;
+    private String nombre;
+    private int puntaje;
+
+    public Equipo(String color, String nombre, int puntaje) {
+        this.color = color;
+        this.nombre = nombre;
+        this.puntaje = puntaje;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPuntaje() {
+        return puntaje;
+    }
 }
--- app/src/main/java/edu/upb/omaigad/upbmatch/upb_match/TeamsManager.java
+++ app/src/main/java/edu/upb/omaigad/upbmatch/upb_match/TeamsManager.java
@@ -10,20 +10,21 @@ public class TeamsManager extends Object {
     /*
         Para usar esto:
 
-     app.teamsManager.getTeams(new GetCallback<ParseObject>() {
+     app.teamsManager.getTeams();
+    */
+
+    public void getTeams(CustomSimpleCallback<Equipo> callback) {
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Equipos");
+        query.orderByAscending("Puntaje");
+        query.getInBackground("xWMyZ4YEGZ", new GetCallback<ParseObject>() {
             public void done(ParseObject object, ParseException e) {
                 if (e == null) {
                     // TODOS LOS OBJETOS DEL PARSE.
+
                 } else {
                     // ALGO SE HA ESTIDO CHE
                 }
             }
         });
-    */
-
-    public void getTeams(GetCallback<ParseObject> callback) {
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("Equipos");
-        query.orderByAscending("Puntaje");
-        query.getInBackground("xWMyZ4YEGZ", callback);
     }
 }
