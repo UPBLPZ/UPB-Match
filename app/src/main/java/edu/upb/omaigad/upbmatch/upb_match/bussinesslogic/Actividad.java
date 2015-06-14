d004f68925cc4a30b6e61df5314fd2fe33f343bf
Andy Ibanez <leonnears@gmail.com>
2015-06-14 14:46:15 -0400

    Ya debería poder sacar los Participantes de una Actividad. (actividad.getParticipantes) con el respectivo callback.

--- app/src/main/java/edu/upb/omaigad/upbmatch/upb_match/bussinesslogic/Actividad.java
+++ app/src/main/java/edu/upb/omaigad/upbmatch/upb_match/bussinesslogic/Actividad.java
@@ -1,8 +1,17 @@
 package edu.upb.omaigad.upbmatch.upb_match.bussinesslogic;
 
 import android.media.MediaRouter;
+import android.util.Log;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
 import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by andyibanezk on 6/11/15.
@@ -12,6 +21,11 @@ public class Actividad {
     public class Participante {
         Equipo equipo;
         int puntaje;
+
+        Participante(Equipo equipo, int puntaje) {
+            this.equipo = equipo;
+            this.puntaje = puntaje;
+        }
     }
 
     private String estado;
@@ -54,9 +68,33 @@ public class Actividad {
         this.reglas = reglas;
     }
 
-    public void getParticipantes(CustomSimpleCallback<Participante> participants) {
-        ParseQuery query = ParseQuery.getQuery("Actividades");
-        query.whereEqualTo("objectId", this.ID);
-        query.include("Id_Equipo");
+    public void getParticipantes(final CustomSimpleCallback<Participante> callback) {
+        ParseQuery query = ParseQuery.getQuery("Participantes");
+        query.whereEqualTo("Id_Actividad", this.ID);
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> list, ParseException e) {
+                if (e == null) {
+                    //Log.e("ANDY PARSE", parseObject.toString());
+
+                    ArrayList<Participante> partis = new ArrayList<Participante>();
+                    for (ParseObject participant : list) {
+                        ParseObject team = participant.getParseObject("Id_Equipo");
+                        String tName = team.getString("Nombre_Equipo");
+                        String tColor = team.getString("Color");
+                        int tScore = team.getInt("Puntaje");
+                        int tLost = team.getInt("Puntos_Perdidos");
+                        String id = team.getObjectId();
+                        Equipo indiTeam = new Equipo(tName, tColor, tScore, tLost, id);
+                        Participante dudeBro = new Participante(indiTeam, participant.getInt("puntaje"));
+                        partis.add(dudeBro);
+                    }
+                    callback.done(partis);
+                } else {
+                    // TODO: Pasar el cache cuando relevante
+                    callback.fail("Fail", new ArrayList<Participante>());
+                }
+            }
+        });
     }
 }
